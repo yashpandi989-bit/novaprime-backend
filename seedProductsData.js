d362fba29d38c03d65d37de9b238330dc7af7284
Yash <lightbuddy789@gmail.com>
2026-06-12 00:15:50 +0530

    add seed products api

---
+++ seedProductsData.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: "Rebar Coupler",
+    category: "Couplers",
+    hsn: "7308",
+    gst: 18,
+    rate: 40,
+    stock: 5000,
+    unit: "pcs",
+  },
+  {
+    name: "Rebar Threading Machine",
+    category: "Machines",
+    hsn: "8462",
+    gst: 18,
+    rate: 135000,
+    stock: 25,
+    unit: "unit",
+  }
+];
