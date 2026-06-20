e078c45249659586ef0f42ff2d2be632b0400659
Yash <lightbuddy789@gmail.com>
2026-06-20 13:11:51 +0530

    Restore product seed data

--- seedProductsData.js
+++ seedProductsData.js
@@ -1,20 +1,82 @@
 module.exports = [
   {
-    name: "Rebar Coupler",
+    name: "Rebar Coupler 16mm",
     category: "Couplers",
     hsn: "7308",
     gst: 18,
-    rate: 40,
-    stock: 5000,
+    rate: 21,
+    stock: 0,
     unit: "pcs",
+    status: "Active"
   },
   {
-    name: "Rebar Threading Machine",
-    category: "Machines",
-    hsn: "8462",
+    name: "Threading 16MM Both Site",
+    category: "Threading",
+    hsn: "",
     gst: 18,
-    rate: 135000,
-    stock: 25,
-    unit: "unit",
+    rate: 30,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Threading 20MM Both Site",
+    category: "Threading",
+    hsn: "",
+    gst: 18,
+    rate: 36,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Threading 25MM Both Site",
+    category: "Threading",
+    hsn: "",
+    gst: 18,
+    rate: 40,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Threading 32MM Both Site",
+    category: "Threading",
+    hsn: "",
+    gst: 18,
+    rate: 42,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Rebar Coupler 20mm",
+    category: "Couplers",
+    hsn: "7307",
+    gst: 18,
+    rate: 35,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Rebar Coupler 25mm",
+    category: "Couplers",
+    hsn: "",
+    gst: 18,
+    rate: 56,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
+  },
+  {
+    name: "Rebar Coupler 32mm",
+    category: "Couplers",
+    hsn: "7307",
+    gst: 18,
+    rate: 95,
+    stock: 0,
+    unit: "pcs",
+    status: "Active"
   }
 ];
